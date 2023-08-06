10315336ca278944a995f5ad8b4335a549ff11d6
Olga <olga.sleepless@gmail.com>
2023-08-06 18:05:36 -0400

    Add problems

---
+++ prime/prime1.py
@@ -0,0 +1,12 @@
+# Write a function prime that takes in a number and returns a boolean, 
+# indicating whether the number is prime. 
+# A prime number is only divisible by 1 and itself.
+def prime(num):
+    # your code here
+
+print(prime(2))  # True
+print(prime(5))  # True
+print(prime(11)) # True
+print(prime(4))  # False
+print(prime(9))  # False
+print(prime(-5)) # False
