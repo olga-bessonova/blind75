7788ee1be876ce2909671a53e42e048b54ff4b56
Olga <olga.sleepless@gmail.com>
2023-08-09 15:23:52 -0400

    Add problems

---
+++ consonant_cancel/_consonant_cancel.py
@@ -0,0 +1,7 @@
+#Write a function consonant_cancel that takes in a sentence and 
+# returns a new sentence where every word begins with it's first vowel.
+def consonant_cancel(sentence):
+          
+
+print(consonant_cancel("down the rabbit hole")) # "own e abbit ole"
+print(consonant_cancel("writing code is challenging")) # "iting ode is allenging"
---
+++ consonant_cancel/consonant_cancel1.py
@@ -0,0 +1,27 @@
+#Write a function consonant_cancel that takes in a sentence and 
+# returns a new sentence where every word begins with it's first vowel.
+def consonant_cancel(sentence):
+    arr = sentence.split(' ')
+    words = list(map(lambda word: consonant_cancel_word(word), arr))
+    
+    return ' '.join(words)
+# def consonant_cancel(sentence):
+#     arr = sentence.split(' ')
+#     for i in range(len(arr)):
+#         arr[i] = consonant_cancel_word(arr[i])
+#     return ' '.join(arr)
+
+      
+def consonant_cancel_word(word):
+    vowels = 'aeiou'
+    for i, ch in enumerate(word):
+        if ch in vowels:
+            return word[i:len(word)]
+    return None       
+          
+
+print(consonant_cancel("down the rabbit hole")) # "own e abbit ole"
+print(consonant_cancel("writing code is challenging")) # "iting ode is allenging"
+# print(consonant_cancel_word('dowm'))
+# print(consonant_cancel_word('iiiowm'))
+# print(consonant_cancel_word('huhuowm'))
