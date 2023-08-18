74b86ef058be00734c44d73644ae2392a24d955d
Olga <olga.sleepless@gmail.com>
2023-08-18 18:34:06 -0400

    Add new problems

--- set_matrix_zero/set_matrix.zero1.py
+++ set_matrix_zero/set_matrix.zero1.py
@@ -1,69 +1,79 @@
 # Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        nrows = len(matrix)
-        ncols = len(matrix[0])
-        if matrix[0][0] == 0:
-            first_element = True
-        else:
-            first_element = False
+def setZeroes(matrix):
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    nrows = len(matrix)
+    ncols = len(matrix[0])
+    first_element = False
+    first_row = False # true if any cell of the 1st row == 0
+    first_column = False # true if any cell of the 1st column == 0
 
-        print('first element', first_element)
-        first_row = False # true if any cell of the 1st row == 0
-        first_column = False # true if any cell of the 1st column == 0
-        # 1. go through all cells, find 0 and update 1st row, 1st column w/o 1st cell [0,0]
-        # update first_column
-        for r in range(nrows):
-            if matrix[r][0] == 0:
-                first_column = True
-        # update first_row
+    # 1. go through all cells, find 0 and update 1st row, 1st column w/o 1st cell [0,0]
+    # update first_column
+    for r in range(nrows):
+        if matrix[r][0] == 0:
+            first_column = True
+    # update first_row
+    for c in range(ncols):
+        if matrix[0][c] == 0:
+            first_row = True
+    
+    # update first_column
+    for r in range(nrows):
         for c in range(ncols):
-            if matrix[0][c] == 0:
-                first_row = True
-        print('first element', first_element)
-        # update first_column
+            if matrix[r][c] == 0:
+                # if nrows == 1:
+                    # first_row_onerow = True
+                if r==0 and c==0:
+                    first_element = True
+                # print((r,c))
+                matrix[0][c] = 0
+                # print(matrix)
+                
+                if r > 0:
+                    matrix[r][0] = 0                        
+    # 2. A reduced matrix - a matrix w/o 1st row and column
+    # go through a reduced matrix
+    # print(matrix)
+    for r in range(1,nrows):
+        for c in range(1,ncols):
+            if matrix[0][c] == 0 or matrix[r][0] == 0:
+                matrix[r][c] = 0
+    # print(matrix)
+
+    # if matrix[0][0] == 0:
+    #     for r in range(nrows):
+    #         matrix[r][0] = 0
+    # if first_element == True:
+    #     for c in range(ncols):
+    #         matrix[0][c] = 0
+
+    # print(first_row_onerow)
+
+    # if first_row_onerow == True:
+        # for c in range(ncols):
+            # matrix[0][c] = 0
+    if first_element == True:
         for r in range(nrows):
-            for c in range(ncols):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    if r > 0:
-                        matrix[r][0] = 0                        
-                    else:
-                        first_element = True
-                        
-                   
+            matrix[r][0] = 0
+        for c in range(ncols):
+            matrix[0][c] = 0
+    
+    if first_column == True:
+        for r in range(nrows):
+            matrix[r][0] = 0
+    if first_row == True:
+        for c in range(ncols):
+            matrix[0][c] = 0
+    return matrix
 
-                    
-        print('first element', first_element)
-        print('1',matrix)
-        # 2. A reduced matrix - a matrix w/o 1st row and column
-        # go through a reduced matrix
-        
-        for r in range(1,nrows):
-            for c in range(1,ncols):
-                if matrix[0][c] == 0 or matrix[r][0] == 0:
-                    matrix[r][c] = 0
-        print('2 ',matrix)
-        print('first element', first_element)
-        if first_element == True:
-            for r in range(nrows):
-                matrix[r][0] = 0
-            for c in range(ncols):
-                matrix[0][c] = 0
-        print('3 ',matrix)
-        
-        if first_column == True:
-            for r in range(nrows):
-                matrix[r][0] = 0
-        print('4 ',matrix)
-        if first_row == True:
-            for c in range(ncols):
-                matrix[0][c] = 0
-        print('5 ',matrix)
 
+matrix = [[1,1,1],[1,0,1],[1,1,1]]
+print(setZeroes(matrix)) #[[1,0,1],[0,0,0],[1,0,1]]
+
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+print(setZeroes(matrix)) #[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
     
 
