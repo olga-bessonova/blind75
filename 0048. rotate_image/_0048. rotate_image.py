adef24dd00fd0afe61db6caa9e65dc63c6276f9e
Olga <olga.sleepless@gmail.com>
2023-08-28 16:02:38 -0400

    Add new problems

--- 0048. rotate_image/_0048. rotate_image.py
+++ 0048. rotate_image/_0048. rotate_image.py
@@ -3,7 +3,31 @@
 # You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
 # DO NOT allocate another 2D matrix and do the rotation.
 
-def rotate(matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(0, r-l):
+                t, b = l, r
+                # print('i = ',i)
+                t_l_cell = matrix[t+i][l]
+                # print(t_l_cell) 
+                matrix[t+i][l] = matrix[b][l+i]
+                matrix[b][l+i] = matrix[b-i][r]
+                matrix[b-i][r] = matrix[t][r-i]
+                matrix[t][r-i] = t_l_cell
+                print(matrix)
+
+            l += 1
+            r -= 1
+            
+
+
+# def rotate(matrix):
   
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
