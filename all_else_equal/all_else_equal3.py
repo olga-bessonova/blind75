0404edf8b053c0059297cbc3381a1c85aaf794da
Olga <olga.sleepless@gmail.com>
2023-08-31 15:34:05 -0400

    Add new problems

---
+++ all_else_equal/all_else_equal3.py
@@ -0,0 +1,14 @@
+# Write a function all_else_equal that takes in a list of numbers. 
+# The function should return the element of the list that is equal 
+# to half of the sum of all elements of the list. 
+# If there is no such element, the method should return None.
+def all_else_equal(num_list):
+    hs = sum(num_list) / 2
+    for i in num_list:
+        if i == hs:
+            return i
+    return None 
+
+print(all_else_equal([2, 4, 3, 10, 1])) # 10
+print(all_else_equal([6, 3, 5, -9, 1])) # 3
+print(all_else_equal([1, 2, 3, 4]))     # None
