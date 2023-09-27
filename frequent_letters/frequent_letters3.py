a2b7c969b9dfe158172c831448e8f21db0c03e53
Olga <olga.sleepless@gmail.com>
2023-09-28 00:00:06 +0300

    Add new problems

--- frequent_letters/frequent_letters3.py
+++ frequent_letters/frequent_letters3.py
@@ -2,8 +2,13 @@
 # returns a list containing the characters that appeared more than twice in a string.
 
 def frequent_letters(string):
-  for i in range(len(string) - 1):
-    
+  hash = {}
+  for i, ch in enumerate(string):
+    if ch in hash:
+      hash[ch] += 1
+    else:
+      hash[ch] = 1
+  return [x for x in hash.keys if hash[x] > 2]
 
 print(frequent_letters("mississippi")) #["i", "s"]
 print(frequent_letters("bootcamp")) #[]
