6e06f79f55bc0ea979c8afffce470697f1749dc0
Olga <olga.sleepless@gmail.com>
2023-10-19 23:49:03 +0300

    Add more practice

--- perfect_square/perfect_square3.py
+++ perfect_square/perfect_square3.py
@@ -3,11 +3,16 @@
 # results from multiplying a number by itself. For example, 9 is a perfect square
 # because 3 x 3 = 9, and 25 is a perfect square because 5 x 5 = 25.
 def perfect_square(num):
+  for i in range(num % 2):
+    if num == i * i:
+      print i
+      return True
+  return False
 
-print(perfect_square(5))   # False
-print(perfect_square(12))  # False
-print(perfect_square(30))  # False
+# print(perfect_square(5))   # False
+# print(perfect_square(12))  # False
+# print(perfect_square(30))  # False
 print(perfect_square(9))   # True
-print(perfect_square(25))  # True
+# print(perfect_square(25))  # True
 
 # print(43 // 2)
