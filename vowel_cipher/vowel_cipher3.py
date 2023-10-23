7d2b9b017d2517eb51143e23c41d3a1c478ab73b
Olga <olga.sleepless@gmail.com>
2023-10-23 21:48:42 +0300

    Add more practice

---
+++ vowel_cipher/vowel_cipher3.py
@@ -0,0 +1,9 @@
+# Write a function vowel_cipher that takes in a string and 
+# returns a new string where every vowel becomes the next vowel in the alphabet.
+
+def vowel_cipher(string):
+  
+   
+    
+print(vowel_cipher("bootcamp")) # "buutcemp"
+print(vowel_cipher("paper cup")) # "pepir cap"
