e054a187adc37cc7389a86c4bb440a59216272f8
Olga <olga.sleepless@gmail.com>
2023-11-04 18:58:54 -0400

    Add more practice

--- caesar_cipher/caesar_cipher4.py
+++ caesar_cipher/caesar_cipher4.py
@@ -8,7 +8,11 @@
 def ceasar_cipher(string, num):
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   arr = list(string)
+  for i,ch in enumerate(arr):
+    index = alphabet.index(ch)
+    arr[i] = alphabet[(index + num) % 26]
 
+  return ''.join(arr)
    
 print(ceasar_cipher("apple", 1))    # "bqqmf"
 print(ceasar_cipher("bootcamp", 2)) # "dqqvecor"
