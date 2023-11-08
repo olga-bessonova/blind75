5dfcc8c71d8ee513df5bdf3526cc85087a542a0d
Olga <olga.sleepless@gmail.com>
2023-11-07 19:37:29 -0500

    Add more practice

--- consonant_cancel/consonant_cancel4.py
+++ consonant_cancel/consonant_cancel4.py
@@ -1,6 +1,13 @@
 #Write a function consonant_cancel that takes in a sentence and 
 # returns a new sentence where every word begins with it's first vowel.
 def consonant_cancel(sentence):
+
+def word_changer(word):
+  vowels = 'aeiou'
+  for i in range(len(word)):
+    if word[i] in vowels:
+      return 
+
           
 
 print(consonant_cancel("down the rabbit hole")) # "own e abbit ole"
