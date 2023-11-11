babec7625e63b9a4c670c959085bd8a9ed20e0a2
Olga <olga.sleepless@gmail.com>
2023-11-11 08:02:07 -0500

    Add more practice

--- double_letter_count/double_letter_count4.py
+++ double_letter_count/double_letter_count4.py
@@ -2,8 +2,11 @@
 # returns the number of times that the same letter repeats twice in a row.
 
 def double_letter_count(string):
+  count = 0
   for i in range(len(string)-1):
-    
+    if string[i] == string[i+1]:
+      count += 1
+  return count
    
   
 print(double_letter_count("the jeep rolled down the hill")) # 3
