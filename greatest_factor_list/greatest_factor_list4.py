52836455cdce56da8fd8d4ce0aedde2fcbc9a736
Olga <olga.sleepless@gmail.com>
2023-11-14 19:42:33 -0500

    Add more practice

--- greatest_factor_list/greatest_factor_list4.py
+++ greatest_factor_list/greatest_factor_list4.py
@@ -6,6 +6,11 @@
 # because that would be too easy).
 
 def greatest_factor_list(num_list):
+  return [x/2 if x % 2 == 0 else x for x in num_list]
+
+# def greatest_factor(num):
+#   for i in range(num/2):
+
     
 print(greatest_factor_list([16, 7, 9, 14])) # [8, 7, 9, 7]
 print(greatest_factor_list([30, 3, 24, 21, 10])) # [15, 3, 12, 21, 5]
