219c05829e1e493cc2d0a25ec05cb763da5b17d0
Olga <olga.sleepless@gmail.com>
2023-11-21 19:18:42 -0500

    Add more practice

---
+++ most_vowels/most_vowels4.py
@@ -0,0 +1,24 @@
+# Write a function most_vowels that takes in a sentence 
+# string and returns the word of the sentence that contains the most vowels.
+def most_vowels(sentence):
+  arr = sentence.split()
+  # print(arr)
+  newarr = [vow(x) for x in arr]
+  print(newarr[max(newarr)])
+  # indexof = newarr.max()
+  return arr[newarr[max(newarr)]]
+    
+def vow(word):
+  vowels = 'aeiou'
+  res = 0
+  for i in range(len(word)):
+    if word[i] in vowels:
+      res += 1
+  return res
+
+print(most_vowels("what a wonderful life")) # "wonderful"
+print(most_vowels("what a wonderful lifeeeeeeeee")) # "lifeeeeeeeee"
+
+# arr = [1,2,5,8,90,1]
+# print(arr.max())
+# print(arr.index(max(arr)))
