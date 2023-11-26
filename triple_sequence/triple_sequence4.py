e3145cd4d7a29b255fb3b6ef4e3f7b4fec310ada
Olga <olga.sleepless@gmail.com>
2023-11-25 21:35:22 -0500

    Add practice

--- triple_sequence/triple_sequence4.py
+++ triple_sequence/triple_sequence4.py
@@ -3,7 +3,12 @@
 # with start and is length elements long. In the sequence, every element
 # should be 3 times the previous element. Assume that the length is at least 1.
 def triple_sequence(start, length): 
-
+  i = length
+  res = [start]
+  while i > 1:
+    res.append(res[-1] * 3)
+    i -= 1
+  return res
 
 print(triple_sequence(2, 4)) # [2, 6, 18, 54]
 print(triple_sequence(4, 5)) # [4, 12, 36, 108, 324]
