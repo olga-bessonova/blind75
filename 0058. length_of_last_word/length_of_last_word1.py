55552bb31ffe2203aabe21b90b55ba671cf05f40
Olga <olga.sleepless@gmail.com>
2023-12-23 14:44:55 -0500

    Update length of last word

--- 0058. length_of_last_word/length_of_last_word1.py
+++ 0058. length_of_last_word/length_of_last_word1.py
@@ -1,7 +1,21 @@
+# runtime O(n), memory O(1)
 def lengthOfLastWord(s):
-    arr = s.split(' ')
-    filtered_arr = [el for el in arr if el != '']
-    return len(filtered_arr[-1])
+    i, length = len(s) - 1, 0
+
+    while s[i] == ' ':
+        i -= 1
+    # print("III: ",i)
+    for j in reversed(range(0,i+1)):
+        # print("j: ", j)
+        if s[j] == ' ':
+            # print('break')
+            break
+        else:
+            length += 1
+        # print("i: ", i)
+        # print("length: ", length)
+    # print("length final: ", length)
+    return length
 
 
 print(lengthOfLastWord("Hello World")) # 5
