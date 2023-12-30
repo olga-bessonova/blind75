679b40a7cbf88f66d55dd9ad2793a9983d30cf52
Olga <olga.sleepless@gmail.com>
2023-12-30 12:12:35 -0500

    Add 0014. longest_common_prefix

---
+++ 0014. longest_common_prefix/longest_common_prefix1.py
@@ -0,0 +1,20 @@
+# runtime O(nxm), memory O(n)
+def longestCommonPrefix(strs):
+      prefix = strs[0]
+      r = len(prefix)
+
+      for i in range(1,len(strs)):
+
+          while r > 0:
+              print("i, r, prefix: ", i, r, prefix)
+              if prefix[0:r] == strs[i][0:min(len(strs[i]), r)]:
+                  prefix = prefix[0:r]
+                  break
+              else:
+                  r -= 1
+          prefix = prefix[0:r]
+          if prefix == '':
+              return prefix
+      return prefix
+print(longestCommonPrefix(["abca","aba","aaab"])) # 'a'
+print(longestCommonPrefix(["flower","flower","flower","flower"])) # 'flower'
