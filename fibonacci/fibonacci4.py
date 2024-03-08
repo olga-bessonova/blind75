d2ed276e595300808c737f69d45c6436b087d766
Olga <olga.sleepless@gmail.com>
2024-03-08 10:25:17 -0500

    Add leetcode problems

---
+++ fibonacci/fibonacci4.py
@@ -0,0 +1,23 @@
+# The fibonacci sequence is a sequence of numbers whose first and second elements are 1. 
+# To generate further elements of the sequence we take the sum of the previous two elements. 
+# For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. 
+# Write a function fibonacci that takes in a number length and 
+# returns the fibonacci sequence up to the given length.
+
+def fibonacci(length):
+  if length == 0:
+    return []
+  elif length == 1:
+    return [1]
+  elif length == 2:
+    return [1, 1]
+  else:
+    res = fibonacci(length - 1)
+    res.append(res[-1] + res[-2])
+    return res
+       
+
+print(fibonacci(0)) # []
+print(fibonacci(1)) # [1]
+print(fibonacci(6))  # [1, 1, 2, 3, 5, 8]
+print(fibonacci(8))  # [1, 1, 2, 3, 5, 8, 13, 21]
