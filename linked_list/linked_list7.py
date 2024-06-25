719cdf010a7be51cde58094d1b0798d8191f5fa6
Olga <olga.sleepless@gmail.com>
2024-06-25 19:41:55 -0400

    Add linked list practice

--- linked_list/linked_list7.py
+++ linked_list/linked_list7.py
@@ -11,5 +11,55 @@ class LinkedList:
     curr = self.head
     if not curr:
       self.head = Node(value)
+      return
+
+    while curr.next is not None:
+      curr = curr.next
+
+    curr.next = Node(value)
+  
+  
+  def traverse(self):
+    curr = self.head
+    if curr is None:
+      return None
     
-     
+    while curr is not None:
+      print(curr.value, end=' -> ')
+      curr = curr.next
+  
+
+  def delete(self, remove_value):
+    curr = self.head
+    prev = None
+    if curr is None:
+      return 'The Linked List is empty'
+    
+    if curr.value == remove_value:
+      self.head = curr.next
+      print(remove_value, 'was removed')
+      return self.traverse()
+    while curr is not None:
+      if curr.value == remove_value:
+        prev.next = curr.next
+        print(remove_value, 'was removed')
+        return self.traverse()
+      else:
+        prev = curr
+        curr = curr.next
+    return print(remove_value, 'was not found')
+    
+    
+l = LinkedList()
+l.append('a')
+l.append('b')
+l.append('c')
+l.append('d')
+l.append('e')
+l.append('f')
+l.append('g')
+print(l.traverse())
+
+print(l.delete('a'))
+
+    
