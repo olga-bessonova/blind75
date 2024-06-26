eea6bae4b4b02d268ffb87dd38329d044220b858
Olga <olga.sleepless@gmail.com>
2024-06-26 12:27:14 -0400

    Add tree practice

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,14 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+  if (inorder.length){
+    let index = inorder.indexOf(preorder.shift());
+    const root = new TreeNode(inorder[index])
+    root.left = buildTree(preorder, inorder.slice(0, index));
+    root.right = buildTree(preorder, inorder.slice(index+1));
+
+    return root;
+  }
+  return null;
 
 }
