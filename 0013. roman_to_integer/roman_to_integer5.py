be47e5616eb1da9ec4f9a419b1630c1320f1848f
Olga <olga.sleepless@gmail.com>
2024-08-12 11:56:03 -0400

    Add more python practice

---
+++ 0013. roman_to_integer/roman_to_integer5.py
@@ -0,0 +1,19 @@
+# memory O(1), runtime O(n)
+def romanToInt(s):
+    hash1 = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    hash2 = {'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
+
+    res = 0
+    i = 0
+
+    while i < len(s):
+        if i < len(s) - 1 and s[i:i+2] in hash2:
+            res += hash2[s[i:i+2]]
+            i += 2
+        else:
+            res += hash1[s[i]]
+            i += 1
+      
+    return res
+
+print(romanToInt("MCMXCIV")) # 1994
